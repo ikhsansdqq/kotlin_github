0c4b2e5f31e3cf1b5ac53be56f88ac0db9712179
Ikhsan Assidiqie <ikhsansdqq@gmail.com>
2023-06-24 21:31:46 +0700

    Initial commit

---
+++ app/src/main/java/com/ikhsansdq/kotlingithub/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ikhsansdq.kotlingithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
